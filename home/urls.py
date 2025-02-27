dc261aafb1e43cf0fd11319ab43de34fb743e2a1
KSM017 <84719857+KSM017@users.noreply.github.com>
2025-02-27 23:41:06 +0900

    Delete home directory

--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import login_view, dashboard_view, timetable_view
-
-urlpatterns = [
-    path('', login_view, name='login'),  # 기본 페이지 = 로그인
-    path('dashboard/', dashboard_view, name='dashboard'),  # 로그인 후 이동할 대시보드
-    path('timetable/', timetable_view, name='timetable'),  # 로그인 후 이동할 대시보드
-]

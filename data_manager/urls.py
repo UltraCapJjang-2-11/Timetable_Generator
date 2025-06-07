8fd175f760b9f32e3cbe5160d2ce4143e3ff56c2
KSM017 <84719857+KSM017@users.noreply.github.com>
2025-06-07 16:14:27 +0900

    Delete data_manager directory

--- data_manager/urls.py
+++
@@ -1,9 +0,0 @@
-# data_manager/urls.py
-
-from django.urls import path
-from data_manager.views import CourseSearchView
-
-urlpatterns = [
-    # 예: /data-manager/course/search/
-    path('course/search/', CourseSearchView.as_view(), name='course-search'),
-]
